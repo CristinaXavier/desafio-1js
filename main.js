30e6e5d922f8c5dda2edf35d6be73400a44abd6b
cristina <seu_email@email.com>
2021-07-02 00:34:47 -0300

    jsexercicio

---
+++ main.js
@@ -0,0 +1,15 @@
+let nome="Cristina";
+
+let idade= 52;
+let anoAtual= 2021 - 52
+
+console. log( 'Meu nome é ' + nome + '. E meu ano de nascimento é ' + anoAtual);
+
+let souHumano= true
+let n1= 26
+let n2=26
+
+let soma= n1+n2
+console.log(soma)
+
+
